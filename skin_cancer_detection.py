085080cdbb189600598d14b54f717ef5f49ea4e4
Madhavan-sastra <140817698+Madhavan-sastra@users.noreply.github.com>
2023-07-28 19:22:02 +0530

    Add files via upload

---
+++ skin_cancer_detection.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+from keras.layers import Conv2D, Flatten, Dense, MaxPool2D
+from keras.models import Sequential
+
+classes = {
+    0: ("actinic keratoses and intraepithelial carcinomae(Cancer)"),
+    1: ("basal cell carcinoma(Cancer)"),
+    2: ("benign keratosis-like lesions(Non-Cancerous)"),
+    3: ("dermatofibroma(Non-Cancerous)"),
+    4: ("melanocytic nevi(Non-Cancerous)"),
+    5: ("pyogenic granulomas and hemorrhage(Can lead to cancer)"),
+    6: ("melanoma(Cancer)"),
+}
+
+
+model = Sequential()
+model.add(
+    Conv2D(
+        16,
+        kernel_size=(3, 3),
+        input_shape=(28, 28, 3),
+        activation="relu",
+        padding="same",
+    )
+)
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(Conv2D(32, kernel_size=(3, 3), activation="relu"))
+model.add(Conv2D(64, kernel_size=(3, 3), activation="relu"))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(Conv2D(128, kernel_size=(3, 3), activation="relu"))
+model.add(Conv2D(256, kernel_size=(3, 3), activation="relu"))
+model.add(Flatten())
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(Dense(256, activation="relu"))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(Dense(128, activation="relu"))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(Dense(64, activation="relu"))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(Dense(32, activation="relu"))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(Dense(7, activation="softmax"))
+model.summary()
+model.load_weights("best_model.h5")
